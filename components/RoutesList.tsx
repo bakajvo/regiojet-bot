bf69493ac3bb9ba01292dcc39ea8bddaa526ab7a
Vojtěch Bakaj <bakajvo1@fit.cvut.cz>
2022-03-13 16:55:59 +0100

    fix list keys

--- components/RoutesList.tsx
+++ components/RoutesList.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
     Box,
     Center,
@@ -13,12 +13,12 @@ import {
     useColorModeValue,
     VStack,
 } from '@chakra-ui/react';
-import {ArrowForwardIcon} from '@chakra-ui/icons'
-import {IRoutes} from "../lib/model/IRoutes";
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { IRoutes } from "../lib/model/IRoutes";
 import useAudio from "../lib/hooks/useAudio";
 import useConstants from "../lib/api/useConstants";
-import {ICountry} from "../lib/model/ICountry";
-import {FaArrowRight, FaBus, FaCoffee, FaTrain, FaUsers, FaUserTie} from 'react-icons/fa';
+import { ICountry } from "../lib/model/ICountry";
+import { FaArrowRight, FaBus, FaCoffee, FaTrain, FaUsers, FaUserTie } from 'react-icons/fa';
 
 interface RoutesListProps {
     routes: IRoutes;
@@ -28,13 +28,13 @@ interface RoutesListProps {
 }
 
 const RoutesList: React.FC<RoutesListProps> = (props) => {
-    const {routes, fromLocationId, toLocationId} = props;
+    const { routes, fromLocationId, toLocationId } = props;
 
     const mapper = (data: Array<ICountry>): Record<string, string> => {
         return data
             .map(country => country.cities)
             .flat()
-            .map(city => [[{id: city.id, name: city.name}], city.stations
+            .map(city => [[{ id: city.id, name: city.name }], city.stations
                 .map(station => {
                     return {
                         id: station.id,
@@ -49,9 +49,9 @@ const RoutesList: React.FC<RoutesListProps> = (props) => {
             }, {});
     }
 
-    const {data: locations} = useConstants<Array<ICountry>, Record<string, string>>('locations', mapper);
-    const {playing, toggle} = useAudio('Hallelujah.mp3');
-    const [checking, setChecking] = useState<Record<string, boolean>>({});
+    const { data: locations } = useConstants<Array<ICountry>, Record<string, string>>('locations', mapper);
+    const { playing, toggle } = useAudio('Hallelujah.mp3');
+    const [checking, setChecking] = useState<string[]>([]);
 
     useEffect(() => {
         routes.routes.forEach((route) => {
@@ -60,6 +60,8 @@ const RoutesList: React.FC<RoutesListProps> = (props) => {
             }
         });
     }, [routes, checking]);
+
+    console.log('routes', routes, 'checking', checking)
 
     return (
         <Flex
@@ -76,32 +78,32 @@ const RoutesList: React.FC<RoutesListProps> = (props) => {
                 boxShadow={'lg'}
                 p={6}
                 my={12}>
-                <Heading lineHeight={1.1} fontSize={{base: '2xl', md: '3xl'}} textAlign={'center'}>
+                <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }} textAlign={'center'}>
                     {locations ? locations[fromLocationId] : null}
-                    <ArrowForwardIcon/>
+                    <ArrowForwardIcon />
                     {locations ? locations[toLocationId] : null}
                 </Heading>
                 <Text textAlign={'center'}
-                      fontSize={{base: 'sm', sm: 'md'}}
-                      color={useColorModeValue('gray.800', 'gray.400')}>
+                    fontSize={{ base: 'sm', sm: 'md' }}
+                    color={useColorModeValue('gray.800', 'gray.400')}>
                     {routes.routesMessage}
                 </Text>
                 <Text textAlign={'center'}
-                      fontSize={{base: 'md', sm: 'xl'}}
-                      color={useColorModeValue('gray.800', 'gray.400')}>
+                    fontSize={{ base: 'md', sm: 'xl' }}
+                    color={useColorModeValue('gray.800', 'gray.400')}>
                     {new Date(routes.routes[0].departureTime).toLocaleDateString()}
                 </Text>
                 <Center>
                     <Box>
-                        {routes.routes.map((route, index) =>
-                            <Flex key={index}>
+                        {routes.routes.map((route) =>
+                            <Flex key={route.id}>
                                 <Box>
                                     <VStack>
                                         <HStack spacing={4}>
                                             <Tag size={'md'} variant="solid" colorScheme="gray">
                                                 {(new Date(route.departureTime)).toLocaleTimeString().slice(0, -3)}
                                             </Tag>
-                                            <Icon as={FaArrowRight} color={'gray'}/>
+                                            <Icon as={FaArrowRight} color={'gray'} />
                                             <Tag size={'md'} variant="solid" colorScheme="gray">
                                                 {(new Date(route.arrivalTime)).toLocaleTimeString().slice(0, -3)}
                                             </Tag>
@@ -113,28 +115,24 @@ const RoutesList: React.FC<RoutesListProps> = (props) => {
                                 </Box>
                                 <Box padding={4}>
                                     <HStack>
-                                        <Icon as={FaUsers} color={route.freeSeatsCount > 0 ? 'green.500' : 'red.500'}/>
+                                        <Icon as={FaUsers} color={route.freeSeatsCount > 0 ? 'green.500' : 'red.500'} />
                                         <Text>{route.freeSeatsCount}</Text>
                                     </HStack>
                                 </Box>
                                 <Box minW={20} padding={4}>
                                     <HStack>
-                                        {route.vehicleStandardKey === "FUN_RELAX_SELF_SERVICE" && <Icon as={FaCoffee}/>}
-                                        {route.vehicleStandardKey === "DEUTSCHE_BAHN" && <Icon as={FaCoffee}/>}
-                                        {route.vehicleStandardKey === "YELLOW" && <Icon as={FaUserTie}/>}
-                                        {route.vehicleTypes.includes('BUS') && <Icon as={FaBus}/>}
-                                        {route.vehicleTypes.includes('TRAIN') && <Icon as={FaTrain}/>}
+                                        {route.vehicleStandardKey === "FUN_RELAX_SELF_SERVICE" && <Icon as={FaCoffee} />}
+                                        {route.vehicleStandardKey === "DEUTSCHE_BAHN" && <Icon as={FaCoffee} />}
+                                        {route.vehicleStandardKey === "YELLOW" && <Icon as={FaUserTie} />}
+                                        {route.vehicleTypes.includes('BUS') && <Icon as={FaBus} />}
+                                        {route.vehicleTypes.includes('TRAIN') && <Icon as={FaTrain} />}
                                     </HStack>
                                 </Box>
                                 <Box padding={4}>
-                                    <Switch checked={checking[route.id] || false}
-                                            onChange={(e) =>
-                                                setChecking({
-                                                    ...checking,
-                                                    [route.id]: e.target.checked
-                                                })
-                                            }
-                                            id="email-alerts"
+                                    <Switch checked={checking.includes(route.id)}
+                                        onChange={(e) =>
+                                            e.target.checked ? setChecking([...checking, route.id]) : setChecking(checking.filter(id => id !== route.id))
+                                        }
                                     />
                                 </Box>
                             </Flex>
